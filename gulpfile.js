54cf60b1ad21656d8310419c3978c00d28fb1921
Sergey Sova <i.am@lestad.net>
2016-01-22 00:05:37 +0300

    Build PegJS

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,32 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var rimraf = require('gulp-rimraf');
+var peg = require('gulp-peg');
 
 
 /**
  * Build library
  */
-gulp.task('build', function(){
+gulp.task('build', ['pegjs'], function() {
   return gulp.src(['src/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('lib'));
 });
 
 
-gulp.task('clean', function(){
+gulp.task('pegjs', function() {
+  return gulp.src(['src/*.pegjs'])
+    .pipe(peg())
+    .pipe(gulp.dest('lib'));
+});
+
+
+gulp.task('clean', function() {
   gulp.src(['lib/*'], { read: false })
     .pipe(rimraf());
 });
 
 
-gulp.task('default', ['clean'], function(){
+gulp.task('default', ['clean'], function() {
   gulp.start('build');
 });
--- src/service.js
+++ src/service.js
@@ -1,7 +1,14 @@
 
+import { parse } from './olly';
+
 export default class Olly {
   constructor() {
     this.demo = 'Not implemented!';
+    this.d = 'api 0 { controller demo; }';
+  }
+
+  parse() {
+    console.log(parse(this.d));
   }
 }
 
