e35edea98123d15ef234f072dbf179d66fe7c1ac
Sergey Sova <i.am@lestad.net>
2016-03-17 16:28:23 +0300

    Demo: more controllers

--- demo.js
+++ demo.js
@@ -3,16 +3,21 @@ var olly = require('./lib');
 
 var app = olly('./demo.conf');
 
-app.controller('session', {
-  register(req, res)           { res.json({ register: true }) },
+app.controller('index', {
   approve(req, res)            { res.json({ approve: true }) },
   whitelist(req, res)          { res.json({ whitelist: true }) },
   whitelist_add(req, res)      { res.json({ whitelist_add: true }) },
   whitelist_remove(req, res)   { res.json({ whitelist_remove: true }) },
-  settings(req, res)           { res.json({ settings: true }) },
-  settings_update(req, res)    { res.json({ settings_update: true }) },
 });
 
+app.controller('session', {
+  register(req, res)           { res.json({ register: true }) },
+});
+
+app.controller('settings', {
+  settings(req, res)           { res.json({ settings: true }) },
+  settings_update(req, res)    { res.json({ settings_update: true }) },
+})
 
 
 app.listen(3000, function(){
